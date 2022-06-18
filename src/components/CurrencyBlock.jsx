8e02fe8491b9398b628d06c8d4b7c39393255a5b
vasylcode <vasylcode@gmail.com>
2022-06-18 19:09:06 +0200

    onblur event

--- src/components/CurrencyBlock.jsx
+++ src/components/CurrencyBlock.jsx
@@ -4,7 +4,6 @@ function CurrencyBlock( {name, color, price, onChangeBlock} ) {
 	const image = './images/'+ name +'.svg';
 	const classColor = 'currency-block ' + color;
 	
-	// price = price && price.toFixed(2);
 
 	const [value, setValue] = React.useState();
 
@@ -15,9 +14,9 @@ function CurrencyBlock( {name, color, price, onChangeBlock} ) {
 	const onInputChange = e => {
 		setValue(e.target.value);
 		onChangeBlock(name, e.target.value);
-		// console.log(e.target.nextSibling.innerHTML)
 	}
 
+	// onBlur={e => alert("test")}
   return (
 	<div className={classColor} key={name}>
 		<img src={image} alt='' className='currency-img' />
